304c5965289989e2252d9db1d1125cb2bba7a310
tkoleary <terrenceoleary@gmail.com>
2018-08-03 21:25:58 -0500

    added offline logic

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+Build the project with `ng build --prod --output-path [PROJECT] --base-href "https://[USERNAME].github.io/[PROJECT]/"`
+
+Serve the project to github pages with: `ngh --dir=[PROJECT]`
 
 ## Running unit tests
 
--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -6,6 +6,12 @@ class CatImage {
   fontsize: number;
 }
 
+class Button { 
+  text: string;
+  disabled: boolean;
+  color: string;
+}
+
 @Component({
   selector: 'app-img-card',
   templateUrl: './img-card.component.html',
@@ -20,15 +26,17 @@ export class ImgCardComponent implements OnInit {
     fontsize: 40,
   };
 
-  public src: string;
+  public button: Button = {
+    text: 'Give me another cat',
+    color: 'primary',
+    disabled: false
+  };
 
   ngOnInit() {
-    this.generateSrc();
-  }
-
-  public generateSrc(): void {
   this.src = this.image.api + this.image.message + 
-    '?size=' + this.image.fontsize +
-    '&ts=' + Date.now();
+    '?size=' + this.image.fontsize;
+  if (!navigator.onLine) {
+    this.button.text = 'Sorry, you\'re offline';
+    this.button.disabled = true;
   }
 }
